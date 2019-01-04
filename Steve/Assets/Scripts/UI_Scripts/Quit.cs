e9894030885bb096b83d888c97662f441b120448
Kali <kkytex@gmail.com>
2019-01-04 19:34:00 +0200

    [dev] Start menu edit

---
+++ Steve/Assets/Scripts/UI_Scripts/Quit.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Quit : MonoBehaviour {
+
+	public Button quit_;
+	void Start ()
+	{
+		Button myBtn = quit_.GetComponent<Button>();
+		myBtn.onClick.AddListener (TaskOnClick);
+	}
+	
+	void TaskOnClick()
+	{
+		if(EditorApplication.isPlaying == false)
+			Application.Quit ();
+	}
+}
