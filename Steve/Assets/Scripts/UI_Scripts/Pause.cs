ff5d97178b510fb976b8d7a866dd1c799938fa87
Kali <kkytex@gmail.com>
2019-01-05 15:59:35 +0200

    [dev] Added pause button

---
+++ Steve/Assets/Scripts/UI_Scripts/Pause.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject Joystick;
+    public GameObject RotateJoystick;
+    public Button PauseBttn;
+    public GameObject panel;
+    public GameObject panelOptions;
+    public GameObject panelUpgrade;
+    public Color colorPaused;
+    public Color colorActive;
+
+
+    private bool isPaused = false;
+
+    void Start()
+    {
+        Button btn = PauseBttn.GetComponent<Button>();
+        btn.onClick.AddListener(PauseGame);
+    }
+    public void PauseGame()
+    {
+
+        if (isPaused)
+        {
+            panel.SetActive(false);
+            panelUpgrade.SetActive(false);
+            panelOptions.SetActive(false);
+            PauseBttn.image.color = colorActive;
+            Time.timeScale = 1;
+            isPaused = false;
+
+            Joystick.SetActive(true);
+            RotateJoystick.SetActive(true);
+        }
+        else
+        {
+            panel.SetActive(true);
+            PauseBttn.image.color = colorPaused;
+            Joystick.SetActive(false);
+            RotateJoystick.SetActive(false);
+
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+    }
+}
