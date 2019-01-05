b53fe58ad7fe7a288fee8ebbb4be8d5c4e923017
Calin Sulea <cailn.g.sulea@gmail.com>
2019-01-05 17:51:17 +0200

    [dev] Added enemy controller

---
+++ Steve/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject target;
+    public int speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+    }
+}
