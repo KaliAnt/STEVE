c52b0e471648ebd1e83c0d8e60c1387ef9c7a297
Calin Sulea <cailn.g.sulea@gmail.com>
2019-01-05 19:48:07 +0200

    [dev] Added field of view

---
+++ Steve/Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FieldOfView fow = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fow.transform.position, Vector3.forward, Vector3.up, 360, fow.viewRadius);
+
+        Vector3 viewAngleA = fow.GetDirectionFromAngle(-fow.viewAngle / 2);
+        Vector3 viewAngleB = fow.GetDirectionFromAngle(fow.viewAngle / 2);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+    }
+}
---
+++ Steve/Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewAngle;
+    public float viewRadius;
+    public GameObject parentPlayer;
+
+    public Vector2 GetDirectionFromAngle(float angleInDegrees)
+    {
+
+        float offset = Mathf.Rad2Deg * Mathf.Atan(transform.position.x / transform.position.y);
+        angleInDegrees -= parentPlayer.transform.eulerAngles.z - offset;
+        if(transform.position.y < 0)
+        {
+            angleInDegrees -= 180;
+        }
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
