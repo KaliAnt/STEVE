efa3e4d4574e3a869fc3ef07664478d23c319c6b
Kali <kkytex@gmail.com>
2019-01-07 22:13:54 +0200

    [dev] Made enemy follow player

--- Steve/Assets/Scripts/EnemyController.cs
+++ Steve/Assets/Scripts/EnemyController.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public GameObject target;
-    public int speed;
+    public int speed = 5;
+    public float rotateSpeed = 5f;
+    public float attackRange = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,22 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+        Vector2 direction = target.transform.position;
+        //float angle = Mathf.Atan2(direction.y - transform.position.y, direction.x - transform.position.x) * Mathf.Rad2Deg;
+        // Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.back);
+        //transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
+        float distanceX = direction.x - transform.position.x;
+        float distanceY = direction.y - transform.position.y;
+        if ((Mathf.Abs(distanceX) < attackRange) && (Mathf.Abs(distanceY) < attackRange))
+        {
+            transform.position = Vector2.MoveTowards(transform.position, direction, speed * Time.deltaTime);
+        }
+
+        Vector2 dir = new Vector2(
+            direction.x - transform.position.x,
+            direction.y - transform.position.y
+        );
+
+        transform.up = dir;
     }
 }
